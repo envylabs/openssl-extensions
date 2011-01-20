dadd220b44ffd1602b1e2adc6241547057f12512
Nathaniel Bibler <nbibler@nathanielbibler.com>
2011-01-20 08:55:37 -0800

    Add method documentation.

--- lib/openssl-extensions/pkey/pkey.rb
+++ lib/openssl-extensions/pkey/pkey.rb
@@ -5,12 +5,19 @@ require 'openssl-extensions/pkey'
 # Extends OpenSSL::PKey::PKey and its submodules with helper methods.
 #
 module OpenSSLExtensions::PKey::PKey
+  ##
+  # Equality is tested by comparing the instances' +hash+.
+  #
   def ==(other)
     other.kind_of?(OpenSSL::PKey::PKey) &&
       self.hash == other.hash
   end
   alias_method :eql?, :==
 
+  ##
+  # Override the default Object#hash to identify uniqueness of the key.
+  # This uses a hash of the PEM.
+  #
   def hash
     to_pem.hash
   end
